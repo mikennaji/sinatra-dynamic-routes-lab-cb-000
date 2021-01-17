8d2a5a357e627152a08f825f29c06c957b3399b2
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-17 22:47:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
     @reversename = params[:name]
-    @reversename.reverse
+    @reversename = @reversename.reverse
     "#{@reversename}"
 
   end
