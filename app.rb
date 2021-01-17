aba90a40c204671a86ce513135690ba58cc31327
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-17 22:55:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,8 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
    @number = params[:number].to_i
    @phrase =  params[:phrase]
-   "#{@number}*#{@phrase}"
+   @phrase1 =@number*@phrase
+   "#{@phrase1}"
   end
 
 
