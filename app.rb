13e8944c85e83109f32a454502eaa078227c0d75
Michael Nnaji <mnnaji3@uwo.ca>
2021-01-17 22:45:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get "/reversename/:name" do
+    @reversename = params[:name]
+    @reversename.reverse
+    '#{@reversename}'
+    
+  end
 end
